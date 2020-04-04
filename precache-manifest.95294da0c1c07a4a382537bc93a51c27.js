5b6ae98ed770be9fce5e0880df9c112c2a342cac
R <permadi.afrian@gmail.com>
2020-04-04 18:25:29 +0700

    commit

---
+++ precache-manifest.95294da0c1c07a4a382537bc93a51c27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d52107ece50a502a842313daceadf45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf07610bddd74ebd553d",
+    "url": "/static/css/main.9575c798.chunk.css"
+  },
+  {
+    "revision": "61779fec3b1b27defae4",
+    "url": "/static/js/2.09959e34.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.09959e34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf07610bddd74ebd553d",
+    "url": "/static/js/main.a19c6f91.chunk.js"
+  },
+  {
+    "revision": "ff16c1ae4708576adf8e",
+    "url": "/static/js/runtime-main.fc305133.js"
+  },
+  {
+    "revision": "34b40896d2abe56ff9f56a1b2313350f",
+    "url": "/static/media/cat.34b40896.png"
+  }
+]);
